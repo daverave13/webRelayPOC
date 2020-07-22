89e5b3227acf18009c06880f0d2daecfa0c38b5e
David Slusser <dslusser13@gmail.com>
2020-07-22 09:02:04 -0700

    added setState function and matching POST route

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 //Middleware
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 //Environment variables
 const port = 3000;
@@ -17,6 +18,14 @@ const ipaddress = "10.5.10.100";
 let xmlDoc = new XMLHttpRequest();
 let state = "";
 
+function convertRaw(data) {
+  let output = "";
+  for (let i = 0; i < data.length; i++) {
+    output += String.fromCharCode(data[i]);
+  }
+  return output;
+}
+
 //getState() uses XHR to retrieve the XML data
 //Recieves no parameters
 //Returns XML state of relays
@@ -24,19 +33,40 @@ function getState() {
   xmlDoc.open("GET", "http://" + ipaddress + "/state.xml", false);
   xmlDoc.send(null);
   if (xmlDoc.readyState == 4) {
-    const xmlLength = xmlDoc.statusText.rawPacket.data.length;
     const bytes = xmlDoc.statusText.rawPacket.data;
-    state = "";
-    for (let i = 0; i < xmlLength; i++) {
-      state += String.fromCharCode(bytes[i]);
-    }
-    return state;
+    return convertRaw(bytes);
+  }
+}
+
+function setState(relayNumber, state) {
+  let request;
+  if (relayNumber != 0)
+    request =
+      "http://" +
+      ipaddress +
+      "/state.xml?relay" +
+      relayNumber +
+      "State=" +
+      state;
+  else request = "http://" + ipaddress + "/state.xml?relayState=" + state;
+
+  xmlDoc.open("GET", request, false);
+
+  xmlDoc.send(null);
+  if (xmlDoc.readyState == 4) {
+    const bytes = xmlDoc.statusText.rawPacket.data;
+    return convertRaw(bytes);
   }
 }
 
 //HTTP GET route
 app.get("/getState", (req, res) => {
   res.send(getState());
+});
+
+//HTTP POST route
+app.post("/setState", (req, res) => {
+  res.send(setState(req.body.relay, req.body.state));
 });
 
 //Initialize App
